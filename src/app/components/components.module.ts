c4f18965466ed822d8e1f2fd9d41ba86395def7d
isaquesantos7 <isaquesantos.second.1998@gmail.com>
2024-09-11 18:50:56 -0300

    feat: base modules

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { AngularMateriaModule } from "../angular-material/angular-material.module";
+
+@NgModule({
+    declarations: [],
+    imports: [
+        AngularMateriaModule
+    ],
+    exports: []
+})
+export class ComponentsModule {
+
+}
