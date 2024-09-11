bddd991e0085cafddc89b474f77ca13f6bd99811
isaquesantos7 <isaquesantos.second.1998@gmail.com>
2024-09-11 19:40:13 -0300

    feat: user-details base template

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from "@angular/core";
 import { AngularMateriaModule } from "../angular-material/angular-material.module";
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 @NgModule({
-    declarations: [],
-    imports: [
-        AngularMateriaModule
-    ],
-    exports: []
+  declarations: [
+    UserDetailsComponent
+  ],
+  imports: [
+    AngularMateriaModule
+  ],
+  exports: [
+    UserDetailsComponent
+  ]
 })
 export class ComponentsModule {
 
