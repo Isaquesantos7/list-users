d233f3e3927a6416f13a8e6d7f2405f23ef25b4e
isaquesantos7 <isaquesantos.second.1998@gmail.com>
2024-09-11 19:17:12 -0300

    feat: data and typeds

---
+++ src/app/interfaces/user/user.ts
@@ -0,0 +1,15 @@
+import { IAddress } from "./address";
+import { IStatus } from "./status";
+
+export interface IUser {
+    nome: string;
+    email: string;
+    senha: string;
+    idade: number;
+    endereco: IAddress;
+    telefone: string;
+    ativo: boolean;
+    funcao: string;
+    dataCadastro: string;
+    status: IStatus;
+}
